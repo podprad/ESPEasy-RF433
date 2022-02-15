115604cda92e8e2da26af001f43bb2eeab638d76
Michał Rakoczy <rakomeister@gmail.com>
2022-02-15 22:17:14 +0100

    chore: radiohead plugin #2

--- src/_P126_Radiohead.ino
+++ src/_P126_Radiohead.ino
@@ -1,17 +1,18 @@
 #include "_Plugin_Helper.h"
+
 #ifdef USES_P126
 
+#include "src/Globals/EventQueue.h"
 #include <RH_ASK.h>
 #include "SPI.h"
 
 // #######################################################################################################
-// #################################### Plugin 033: Dummy ################################################
+// #################################### Plugin 126: RF 433 Events ########################################
 // #######################################################################################################
 
 #define PLUGIN_126
 #define PLUGIN_ID_126 126
-#define PLUGIN_NAME_126 "Custom - Radiohead RX"
-#define PLUGIN_VALUENAME1_126 "Value1"
+#define PLUGIN_NAME_126 "Custom - RF 433 Events"
 
 RH_ASK* driverRef = NULL;
 uint8_t buffer[128];
@@ -28,13 +29,13 @@ boolean Plugin_126(uint8_t function, struct EventStruct *event, String &string)
   {
     Device[++deviceCount].Number = PLUGIN_ID_126;
     Device[deviceCount].Type = DEVICE_TYPE_DUAL;
-    Device[deviceCount].VType = Sensor_VType::SENSOR_TYPE_STRING;
+    Device[deviceCount].VType = Sensor_VType::SENSOR_TYPE_NONE;
     Device[deviceCount].Ports = 0;
     Device[deviceCount].PullUpOption = false;
     Device[deviceCount].InverseLogicOption = false;
     Device[deviceCount].FormulaOption = false;
-    Device[deviceCount].ValueCount = 1;
-    Device[deviceCount].SendDataOption = true;
+    Device[deviceCount].ValueCount = 0;
+    Device[deviceCount].SendDataOption = false;
     Device[deviceCount].TimerOption = false;
     Device[deviceCount].TimerOptional = true;
     Device[deviceCount].GlobalSyncOption = true;
@@ -48,20 +49,13 @@ boolean Plugin_126(uint8_t function, struct EventStruct *event, String &string)
     break;
   }
 
-  case PLUGIN_GET_DEVICEVALUENAMES:
-  {
-    // FIXME TD-er: Copy names as done in P026_Sysinfo.ino.
-    strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_126));
-    break;
-  }
-
   case PLUGIN_INIT:
   {
     int rxPin = Settings.TaskDevicePin1[event->TaskIndex];
     int txPin = Settings.TaskDevicePin2[event->TaskIndex];
 
     if (rxPin < 0 || txPin < 0){
-      addLogMove(LOG_LEVEL_INFO, F("Radiohead: no pins selected"));
+      addLogMove(LOG_LEVEL_INFO, F("RF 433 Events: no pins selected"));
       success = false;
       break;
     }
@@ -69,16 +63,26 @@ boolean Plugin_126(uint8_t function, struct EventStruct *event, String &string)
     driverRef = new RH_ASK(2000, rxPin, txPin);
 
     if (!driverRef->init()){
-      addLogMove(LOG_LEVEL_INFO, F("Radiohead: init failed"));
+      addLogMove(LOG_LEVEL_INFO, F("RF 433 Events: init failed"));
       delete driverRef;
       driverRef = NULL;
       success = false;
       break;      
     }
 
-    addLogMove(LOG_LEVEL_INFO, F("Radiohead: init ok"));
+    addLogMove(LOG_LEVEL_INFO, F("RF 433 Events: init ok"));
 
     success = true;
+    break;
+  }
+
+  case PLUGIN_EXIT:
+  {
+    if (driverRef != NULL){
+      delete driverRef;
+      driverRef = NULL;
+    }
+    
     break;
   }
 
@@ -93,24 +97,23 @@ boolean Plugin_126(uint8_t function, struct EventStruct *event, String &string)
     memset(&buffer, 0, sizeof(buffer));
     
     if (driverRef->recv(buffer, &bufferLength)){
-      addLogMove(LOG_LEVEL_INFO, (char*)buffer);
-      Scheduler.schedule_task_device_timer(event->TaskIndex, millis() + 10);
+      String eventString;
+      eventString.concat((char*)buffer, bufferLength);
+      eventString.trim();
+      eventString.toLowerCase();
+
+      addLogMove(LOG_LEVEL_INFO, String(F("RF 433 Events: received ")) + eventString);
+      
+      String command = parseString(eventString, 1);
+      if (command == F("event")){
+        String newEvent = parseStringToEnd(eventString, 2);
+        addLogMove(LOG_LEVEL_INFO, String(F("RF 433 Events: Trigerring event ")) + newEvent);
+        eventQueue.addMove(std::move(newEvent));
+      }
     }
 
     break;
   }  
-
-  case PLUGIN_READ:
-  {
-    addLogMove(LOG_LEVEL_INFO, F("Radiohead: PLUGIN_READ"));
-
-    event->String2.clear();
-    event->String2.concat((char*)buffer, (int)bufferLength);
-
-    addLogMove(LOG_LEVEL_INFO, event->)
-
-    break;
-  }
   
   }
   return success;
